2ad4ac3d231a0e5dd0bcd0a42d70e952be298699
Lupin <lp1@lp1.eu>
2017-01-14 04:13:56 +0100

    finished game mechanic, missing win/lose check

--- ttt.c
+++ ttt.c
@@ -1,38 +1,46 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <math.h>
 
 #define STDIN 0
 #define STDOUT 1
 #define BUFFER_SIZE 512
-#define BOARD_SIZE 3
+#define ROW_SIZE 3
+#define BOARD_SIZE ROW_SIZE*ROW_SIZE
 
-void clear_board(char *board[BOARD_SIZE][BOARD_SIZE]){
-  for (char i = 0; i < BOARD_SIZE; i++){
-    for (char j = 0; j < BOARD_SIZE; j++){
-      &board[i][j] = '0';
-    }
+void clear_board(char *board){
+  for (int i = 0; i < BOARD_SIZE; i++){
+    board[i] = '0';
   }
 }
 
-void print_board(char *board[BOARD_SIZE][BOARD_SIZE]){
-  for (char i = 0; i < BOARD_SIZE; i++){
-    write(STDOUT, "|", 1);
-    for (char j = 0; j < BOARD_SIZE; j++){
-      write(STDOUT, board[i][j], 1);
-      write(STDOUT, "|", 1);
-    }
-    write(STDOUT, "\n", 1);
+void print_board(char *board){
+  for (int i = 0; i < BOARD_SIZE; i++){
+    write(STDOUT, &board[i], 1);
+    if (i > 0 && (i + 1) % ROW_SIZE == 0)
+      write(STDOUT, "\n", 2);
   }
 }
 
-void process_input(char *board[BOARD_SIZE][BOARD_SIZE], int *input, int *player){
-  print_board(*board);
+void process_input(char *board, int input, int *player){
+  char playerChar = 'X';
+  if (input > BOARD_SIZE)
+    write(STDOUT, "INPUT ERROR\n", 12);
+  else if (board[input] != '0')
+    write(STDOUT, "INVALID MOVE\n", 13);
+  else {
+    if (*player == 1)
+      playerChar = 'O';
+    board[input] = playerChar;
+    *player = !(*player);
+  }
+  print_board(board);
 }
 
 int game(){
   char buffer[BUFFER_SIZE];
-  char board[BOARD_SIZE][BOARD_SIZE];
+  char board[BOARD_SIZE];
   int userInput;
   int stop = 0;
   int player = 0;
@@ -41,7 +49,7 @@ int game(){
   print_board(&board);
   while (read(STDIN, buffer, BUFFER_SIZE) && !stop){
     userInput = atoi(buffer);
-    process_input(&board, &userInput, &player);
+    process_input(&board, userInput, &player);
   }
 }
 
